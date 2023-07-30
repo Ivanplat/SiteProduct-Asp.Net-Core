f9eea926cd9309d98d359341daa8f8b08dfe875d
Ivan Platonov <Platonov20030603@gmail.com>
2023-07-30 11:34:36 +0300

    Added UserController

---
+++ SiteProduct/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiteProduct.Controllers
+{
+    [Route("{controller}/{action=Index}/{id?}/{*catchall}")]
+    public class UserController : Controller
+    {
+        public string Index()
+        {
+            string result = string.Empty;
+            RouteData data = HttpContext.GetRouteData();
+            foreach(var item in data.Values)
+            {
+                result += $"{item.Key}={item.Value}\n";
+            }
+            return result;
+        }
+    }
+}
